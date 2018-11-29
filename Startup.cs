b7fbafe9d76e7a9e57d8e32d1f6b87035fa7fbe5
Abdihakin Abdullahi <xakiin6@gmail.com>
2018-11-29 18:11:02 +0000

    first Commit

---
+++ Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MVCWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+  services.AddIdentityServer()
+        .AddDeveloperSigningCredential()
+        .AddInMemoryIdentityResources(Config.GetIdentityResources())
+        .AddInMemoryApiResources(Config.GetApiResources())
+        .AddInMemoryClients(Config.GetClients())
+        .AddTestUsers(Config.GetUsers());
+  services.AddAuthentication()
+        .AddGoogle("Google", options =>
+        {
+            options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+            
+            options.ClientId = "718881823908-icgdba0lcfi7cmqbg9a9aj3fbm7ae6ng.apps.googleusercontent.com";
+            options.ClientSecret = "hcbr1AxUpTJXTy37m9AUPPiN";
+        });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+app.UseIdentityServer();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ config.cs
@@ -0,0 +1,117 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+ namespace MVCWeb
+{
+public class Config {
+
+    public static IEnumerable<ApiResource> GetApiResources()
+{
+    return new List<ApiResource>
+    {
+        new ApiResource("api1", "My API")
+    };
+}
+
+public static List<IdentityResource> GetIdentityResources()
+{
+    return new List<IdentityResource>
+    {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile() // <-- usefull
+    };
+}
+public static IEnumerable<Client> GetClients()
+{
+    return new List<Client>
+    {
+        new Client
+        {
+            ClientId = "client",
+
+            // no interactive user, use the clientid/secret for authentication
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+            // secret for authentication
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256())
+            },
+
+            // scopes that client has access to
+            AllowedScopes = { "api1" }
+        },
+       
+
+        //Development Test Client
+new Client
+{
+    ClientId = "jsDev",
+    ClientName = "JavaScript Client",
+    AllowedGrantTypes = GrantTypes.Implicit,
+    AllowAccessTokensViaBrowser = true,
+    
+    ClientUri =  "http://localhost:4200/",
+    Enabled =true,
+
+    RedirectUris =           { "http://localhost:4200/assets/signin-callback.html",
+                               "http://localhost:4200/assets/silent-callback.html",
+                               "http://localhost:4200/silent-callback"
+     },
+    
+    PostLogoutRedirectUris = { "http://localhost:4200/callback" },
+    AllowedCorsOrigins =     { "http://localhost:4200/" },
+
+    AllowedScopes =
+    {
+       IdentityServerConstants.StandardScopes.OpenId,
+        IdentityServerConstants.StandardScopes.Profile,
+       IdentityServerConstants.StandardScopes.Email,
+       IdentityServerConstants.StandardScopes.OfflineAccess,
+        "api1"
+    },
+
+    RequireConsent = false,
+    AllowOfflineAccess=true,
+    AccessTokenLifetime =(60 * 5),
+    IdentityTokenLifetime=(60 * 5)
+    
+}
+    };
+}
+public static List<TestUser> GetUsers()
+{
+    return new List<TestUser>
+    {
+        new TestUser
+        {
+            SubjectId = "1",
+            Username = "admin",
+            Password = "abdihakin",
+
+            Claims = new []
+            {
+                new Claim("username", "admin"),
+                new Claim("name", "Abdihakin Abdullahi"),
+                new Claim("website", "https://www.mayma.local")
+            }
+        },
+        new TestUser
+        {
+            SubjectId = "2",
+            Username = "Ashley.Carter@ao.com",
+            Password = "Carter112018",
+
+            Claims = new []
+            {
+                new Claim("username", "Ashlay"),
+                new Claim("name", "Ashlay Carter"),
+                new Claim("website", "https://ao.com")
+            }
+        }
+    };
+}
+}
+}
